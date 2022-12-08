74209ce83a1a12e508828287ef441a9897b26847
Per-Gustaf Stenberg <pegge.stenberg@gmail.com>
2022-12-08 10:25:57 +0100

    Fixed response body and random with params

--- oauth2.sh
+++ oauth2.sh
@@ -6,13 +6,19 @@ token_endpoint=$3
 
 tmp_file_with_code=$(mktemp)
 
-code_verifier=$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-128} | head -n 1)
+code_verifier=$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${4:-128} | head -n 1)
 code_challenge=$(echo -n $code_verifier | openssl dgst -sha256 -binary | basenc --base64url -w0 | tr -d '=')
 
 # Handle callback
 echo `python - << EOF
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qsl, urlsplit
+
+HTTP_RESPONSE_BODY = """
+<html><body>
+    <p><b>Code successfull consumed, you can now <a href=\"javascript:window.open('','_self').close();\">close</a> close this window.</b></p>
+</body></html>
+"""
 
 class GetHandler(BaseHTTPRequestHandler):
 
@@ -23,6 +29,7 @@ class GetHandler(BaseHTTPRequestHandler):
         print(code)
         self.send_response(200)
         self.end_headers()
+        self.wfile.write(HTTP_RESPONSE_BODY.encode("utf8"))
         return
 
 server = HTTPServer(('localhost', 9999), GetHandler)
